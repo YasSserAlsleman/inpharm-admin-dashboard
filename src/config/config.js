b6ff8f579bc5bae0cfbfd88c6de0f6a20feabdf6
Yasser Alsleman <yasser.alsleman.17@gmail.com>
2026-04-12 21:13:54 +0400

    fix commit

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // ملف الإعدادات المركزي للروابط والمتغيرات العامة
-export const BASE_API_URL = 'http://localhost:5005/api';  // للـ API (من axiosClient.js)
-//export const BASE_FILE_URL = 'https://inpharm-admin-backend.onrender.com';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)
-export const BASE_FILE_URL = 'http://localhost:5005';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)
+export const BASE_API_URL = 'https://inpharm-admin-backend.onrender.com/api';  // للـ API (من axiosClient.js)
+export const BASE_FILE_URL = 'https://inpharm-admin-backend.onrender.com';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)
+//export const BASE_FILE_URL = 'http://localhost:5005';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)
