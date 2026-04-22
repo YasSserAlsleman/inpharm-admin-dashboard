9f582417eaf2b8ad75da4eaa3655ce2ba3cfdbe5
Yasser Alsleman <yasser.alsleman.17@gmail.com>
2026-04-22 13:53:41 +0400

    deploy

--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,5 @@
 // export const BASE_API_URL = import.meta.env.VITE_API_URL || 'https://inpharm-admin-backend.onrender.com/api';  // للـ API (من axiosClient.js)
 // export const BASE_FILE_URL = import.meta.env.VITE_FILE_URL || 'https://inpharm-admin-backend.onrender.com';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)    
 
-export const BASE_API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5005/api';  // للـ API (من axiosClient.js)
-export const BASE_FILE_URL = import.meta.env.VITE_FILE_URL || 'http://localhost:5005';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)    
+export const BASE_API_URL = import.meta.env.VITE_API_URL || 'http://49.13.227.103:5000/api';  // للـ API (من axiosClient.js)
+export const BASE_FILE_URL = import.meta.env.VITE_FILE_URL || 'http://49.13.227.103:5000';  // للملفات المرفوعة (الصور، الفيديوهات، إلخ)    
